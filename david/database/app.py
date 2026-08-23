ce420441db29aac0045b070d3fbff6277a5668c3
David-S-22 <david2017au@gmail.com>
2026-08-23 16:42:17 +1000

    adding database models, basic read and docker integration

--- david/database/app.py
+++ david/database/app.py
@@ -1,23 +1,49 @@
-from sqlalchemy.orm import mapped_column
+from flask import abort
+from flask import jsonify
+from models import Goal, Feedback, Suggestion, db
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-import datetime
-
-class Base(DeclarativeBase):
-  pass
-
-db = SQLAlchemy(model_class=Base)
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///goal_planner.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(os.getcwd(), "savings.db")}"
 db.init_app(app)
-
-class Goal(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    amount: Mapped[int] = mapped_column(nullable=False)
-    due_date: Mapped[datetime.datetime] = mapped_column(nullable=False)
-
 with app.app_context():
     db.create_all()
+
+@app.route("/goals")
+def get_goals():
+    goals = db.session.execute(db.select(Goal)).scalars().all()
+    if (len(goals) == 0):
+        return abort(404)
+
+    return jsonify(goals)
+
+@app.route("/suggestions")
+def get_suggestions():
+    suggestions = db.session.execute(db.select(Suggestion)).scalars().all()
+    if (len(suggestions) == 0):
+        return abort(404)
+
+    return jsonify(suggestions)
+
+@app.route("/feedbacks")
+def get_feedbacks():
+    feedbacks = db.session.execute(db.select(feedbacks)).scalars().all()
+    if (len(feedbacks) == 0):
+        return abort(404)
+
+    return jsonify(feedbacks)
+
+@app.route("/goal/<int:id>")
+def get_goal(id: int):
+    return db.get_or_404(Goal, id).to_dict()
+
+@app.route("/suggestion/<int:id>")
+def get_suggestion(id: int):
+    db.get_or_404(Suggestion, id).to_dict()
+
+@app.route("/feedback/<int:id>")
+def get_feedback(id: int):
+    db.get_or_404(Feedback, id).to_dict()
+
+app.run(host="0.0.0.0", port=5002)
---
+++ david/database/models.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from sqlalchemy.orm import mapped_column
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import datetime
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+@dataclass
+class Goal(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    amount: Mapped[int] = mapped_column(nullable=False)
+    due_date: Mapped[datetime.datetime] = mapped_column(nullable=False)
+
+@dataclass
+class Suggestion(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    suggestion: Mapped[str] = mapped_column(nullable=False)
+
+@dataclass
+class Feedback(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    feedback: Mapped[str] = mapped_column(nullable=False)
