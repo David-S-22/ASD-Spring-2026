45abca1f3c311e1df4b9d3761592418e039e16e3
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-08-23 21:10:23 +1000

    yippee it works

--- aiden/database/__main__.py
+++ aiden/database/__main__.py
@@ -4,6 +4,6 @@ from .app import app, setup
 
 if __name__ == "__main__":
     setup(os.environ["DB_PATH"])
-    port = int(os.environ["PsORT"])
+    port = int(os.environ["PORT"])
 
     app.run(host="0.0.0.0", port=port)
