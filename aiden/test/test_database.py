ca1c4592be6255ed2687b4fecbe0f7cda1d19cd7
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-08-23 19:24:53 +1000

    Add actual test for anomalies database (#37)

--- aiden/test/test_database.py
+++ aiden/test/test_database.py
@@ -1,9 +1,45 @@
-from database.app import app
+import pytest
+from database.app import app, setup
+from flask.testing import FlaskClient
+from uuid import UUID, uuid4
 
 
-def test_index():
+# Setup pytest and flask test client
+@pytest.fixture(scope="session", autouse=True)
+def one_time_setup():
+    setup(":memory:")
+
+@pytest.fixture
+def client():
     with app.test_client() as client:
-        index = client.get("/")
+        yield client
 
-        assert index.status_code == 200
-        assert index.text == '"index page"\n'
+
+# Tests
+def test_index(client: FlaskClient):
+    resp = client.get("/")
+
+    assert resp.status_code == 200
+    assert resp.text == '"index page"\n'
+
+def test_create_anomaly(client: FlaskClient):
+    json = dict(transaction_id=uuid4(), agent_reason_suspected="beans", is_confirmed_by_user="False")
+
+    # Check we can create the object
+    resp = client.post("/anomaly", json=json)
+
+    assert resp.status_code == 201
+    assert isinstance(create_json := resp.json, dict)
+
+    # Check we can fetch the object we just created
+    resp = client.get("/anomaly/" + create_json["id"])
+
+    assert resp.status_code == 200
+    assert isinstance(resp.json, dict)
+    assert resp.json == create_json
+
+    # Check the expected keys
+    assert UUID(resp.json["id"])
+    assert UUID(resp.json["transaction_id"])
+    assert resp.json["is_confirmed_by_user"] == False
+    assert resp.json["agent_reason_suspected"] == "beans"
