2ec65a9cad8150b86d36c92611953a19883ee7b2
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-08-29 22:31:09 +1000

    neaten tests

--- aiden/test/test_backend.py
+++ aiden/test/test_backend.py
@@ -1,24 +1,43 @@
 import re
 
-import pytest
-import responses
+from pytest import MonkeyPatch, fixture
 from flask.testing import FlaskClient
 from requests import PreparedRequest
+from responses import RequestsMock
 
 from backend.app import app
 from database.app import app as dbapp, setup_database
 
 
-# Setup pytest and flask test client
-@pytest.fixture
+# Tests
+def test_index(client: FlaskClient):
+    resp = client.get("/")
+
+    assert resp.status_code == 200
+    assert isinstance(resp.json, dict)
+    assert resp.json["container"] == "anomalies-backend"
+
+def test_create_anomaly(client: FlaskClient):
+    resp = client.post("/dummy-anomaly")
+    assert resp.text.count("<tr>") == 1
+
+    resp = client.post("/dummy-anomaly")
+    assert resp.text.count("<tr>") == 2
+
+    resp = client.post("/dummy-anomaly")
+    assert resp.text.count("<tr>") == 3
+
+
+# Pytest fixtures
+@fixture
 def client():
     setup_database(":memory:")
 
     with app.test_client() as client:
         yield client
 
-@pytest.fixture(autouse=True)
-def integrate_database(monkeypatch: pytest.MonkeyPatch):
+@fixture(autouse=True)
+def integrate_database(monkeypatch: MonkeyPatch):
     # In order to get the backend client to send requests
     # to the actual database server, we have to intercept
     # the requests via responses and redirect them to the
@@ -39,26 +58,8 @@ def integrate_database(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setenv("ANOMALIES_DB_URL", db_url)
     url = re.compile(rf"^{re.escape(db_url)}/?$")
 
-    with responses.RequestsMock(assert_all_requests_are_fired=False) as rsps:
-        for method in (responses.GET, responses.POST, responses.PATCH, responses.DELETE, responses.OPTIONS):
+    with RequestsMock(assert_all_requests_are_fired=False) as rsps:
+        for method in (RequestsMock.GET, RequestsMock.POST, RequestsMock.PATCH, RequestsMock.DELETE, RequestsMock.OPTIONS):
             rsps.add_callback(method, url, intercept)
 
         yield
-
-# Tests
-def test_index(client: FlaskClient):
-    resp = client.get("/")
-
-    assert resp.status_code == 200
-    assert isinstance(resp.json, dict)
-    assert resp.json["container"] == "anomalies-backend"
-
-def test_create_anomaly(client: FlaskClient):
-    resp = client.post("/dummy-anomaly")
-    assert resp.text.count("<tr>") == 1
-
-    resp = client.post("/dummy-anomaly")
-    assert resp.text.count("<tr>") == 2
-
-    resp = client.post("/dummy-anomaly")
-    assert resp.text.count("<tr>") == 3
