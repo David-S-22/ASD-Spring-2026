ed7c317351136c1d094b6178472878f3c8c06a6d
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-08-29 14:29:36 +1000

    test

--- aiden/backend/app.py
+++ aiden/backend/app.py
@@ -18,7 +18,7 @@ def get_anomaly_rows():
 
 @app.post("/dummy-anomaly")
 def create_dummy_anomaly():
-    anomaly = dto.Anomaly(
+    anomaly = dto.Anomaly( # test does it appear if i edit this line
         id=uuid4(),
         transactfion_id=uuid4(),
         agent_reason_suspected="hello",
