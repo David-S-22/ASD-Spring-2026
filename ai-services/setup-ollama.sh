6ee49bfa50be884dfcd6c3e84779e41597890ca1
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-08-30 20:15:59 +1000

    update script

--- ai-services/setup-ollama.sh
+++ ai-services/setup-ollama.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 #reference: https://stackoverflow.com/a/78501628
 
-/bin/ollama serve &
+ollama serve &
 PID=$!
 
-sleep 5
+echo "[START] waiting for ollama"
+secs_to_wait=5
 
-echo "pulling models"
+while ! ollama list > /dev/null 2>&1; do
+  if [ $secs_to_wait -le 0 ]; then
+    echo "[ERROR] Ollama failed to start" >&2
+    kill $PID 2>/dev/null
+    exit 1
+  fi
+
+  secs_to_wait=$((secs_to_wait - 1))
+  sleep 1
+done
+
+echo "[END]   waiting for ollama"
+echo "[START] pulling models"
+
+# If you want to use extra models, add them here
+# to ensure they are installed in the container
 ollama pull qwen2.5:0.5b
 ollama pull llama3.1:8b
-echo "finished pulling models"
 
+echo "[END]   pulling models"
 wait $PID
