83f5bc002001e6d9a4112432643e1c48f0dbe14e
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-08-30 12:33:11 +1000

    lazy load openai client so it doesn't interfere with tests

--- aiden/backend/services/ollama_api.py
+++ aiden/backend/services/ollama_api.py
@@ -1,23 +1,28 @@
+from functools import lru_cache
+
 from openai import OpenAI
 
 from ..helpers import get_env
 
 
-_implementation_model = get_env("OLLAMA_IMPLEMENTATION_MODEL")
-_review_model = get_env("OLLAMA_REVIEW_MODEL")
-_ollama_url = get_env("OLLAMA_URL")
-_client = OpenAI(base_url=_ollama_url, api_key="ollama", timeout=180)
-
-
 def prompt(system_prompt: str, user_prompt: str, review: bool = False) -> str:
     # We intentionally want the review model to be a bit more deterministic,
-    # so we lower the temperature a little bit
+    # so we lower the temperature a little bit. The client instance is cached
+    # between calls, as a singleton instance.
 
-    response = _client.responses.create(
-        model=_review_model if review else _implementation_model,
+    client = _get_client()
+    temperature = 0.2 if review else 0.8
+    model = get_env("OLLAMA_REVIEW_MODEL" if review else "OLLAMA_IMPLEMENTATION_MODEL")
+
+    response = client.responses.create(
+        model=model,
         instructions=system_prompt,
         input=user_prompt,
         max_output_tokens=300,
-        temperature=0.2 if review else 0.8)
+        temperature=temperature)
 
     return response.output_text.strip()
+
+@lru_cache(maxsize=1)
+def _get_client() -> OpenAI:
+    return OpenAI(base_url=get_env("OLLAMA_URL"), api_key="ollama", timeout=180)
