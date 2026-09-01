0f296c04fbf0f585ab90046932e9bf2996ee9784
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2026-09-01 21:33:00 +1000

    add test

--- aiden/test/test_backend.py
+++ aiden/test/test_backend.py
@@ -1,12 +1,16 @@
 import re
 
+from datetime import datetime
 from pytest import MonkeyPatch, fixture
+from flask import Flask
 from flask.testing import FlaskClient
 from requests import PreparedRequest
 from responses import RequestsMock
 
 from backend.app import app
 from database.app import app as dbapp, setup_database
+from janelle.database.app import setup_app as setup_transactions
+from shared.backend import dto
 
 
 # Tests
@@ -27,6 +31,19 @@ def test_create_anomaly(client: FlaskClient):
     resp = client.post("/dummy-anomaly")
     assert resp.text.count("<tr>") == 3
 
+def test_check_transaction(client: FlaskClient):
+    transaction = dto.Transaction(
+        id=5,
+        amount=9999999,
+        merchant="Slim Shady ATMs",
+        date=datetime.now(),
+        description="we are going to steal your money",
+        category_id=0
+    )
+
+    resp = client.post("check-transaction", json=transaction)
+    assert False, resp.text
+
 
 # Pytest fixtures
 @fixture
@@ -37,27 +54,40 @@ def client():
         yield client
 
 @fixture(autouse=True)
-def integrate_database(monkeypatch: MonkeyPatch):
+def integrate_services(monkeypatch: MonkeyPatch):
     # In order to get the backend client to send requests
     # to the actual database server, we have to intercept
     # the requests via responses and redirect them to the
-    # test_client instance
+    # test_client instance. Same with the transactions db
 
-    def intercept(request: PreparedRequest):
-        with dbapp.test_client() as dbclient:
-            resp = dbclient.open(
-                path = request.path_url,
-                method = request.method,
-                headers = dict(request.headers),
-                data = request.body)
-
-            return resp.status_code, dict(resp.headers), resp.get_data()
+    transactionsapp = setup_transactions()
 
     monkeypatch.setenv("ANOMALIES_DB_URL", "http://mock-database-url/anomalies")
-    url = re.compile(r"^http://mock-database-url/anomalies/?$")
+    monkeypatch.setenv("TRANSACTIONS_DB_URL", "http://mock-transactions-url")
+    monkeypatch.setenv("OLLAMA_MODEL", "billy")
+    monkeypatch.setenv("OLLAMA_URL", "http://mock-ollama-url")
+
+    dburl = re.compile(r"^http://mock-database-url/anomalies/?$")
+    transactionsurl = re.compile(r"^http://mock-transactions-url/.+$")
 
     with RequestsMock(assert_all_requests_are_fired=False) as rsps:
-        for method in (RequestsMock.GET, RequestsMock.POST, RequestsMock.PATCH, RequestsMock.DELETE, RequestsMock.OPTIONS):
-            rsps.add_callback(method, url, intercept)
+        rsps.add_callback(RequestsMock.GET, dburl, lambda r: intercept(dbapp, r))
+        rsps.add_callback(RequestsMock.POST, dburl, lambda r: intercept(dbapp, r))
+        rsps.add_callback(RequestsMock.PATCH, dburl, lambda r: intercept(dbapp, r))
+        rsps.add_callback(RequestsMock.DELETE, dburl, lambda r: intercept(dbapp, r))
+        rsps.add_callback(RequestsMock.OPTIONS, dburl, lambda r: intercept(dbapp, r))
+
+        rsps.add_callback(RequestsMock.GET, transactionsurl, lambda r: intercept(transactionsapp, r))
+        rsps.add_callback(RequestsMock.OPTIONS, transactionsurl, lambda r: intercept(transactionsapp, r))
 
         yield
+
+def intercept(app_to_inject: Flask, request: PreparedRequest):
+    with app_to_inject.test_client() as client:
+        resp = client.open(
+            path=request.path_url,
+            method=request.method,
+            headers=dict(request.headers),
+            data=request.body)
+
+        return resp.status_code, dict(resp.headers), resp.get_data()
